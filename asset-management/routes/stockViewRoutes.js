619e5a916534cfb543291449b2a5c87626f22805
kkreactdev <kkreactdev@gmail.com>
2025-05-13 11:51:29 +0530

    Delete asset-management directory

--- asset-management/routes/stockViewRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stockViewController = require('../controllers/stockViewControllers');
-
-router.get('/', stockViewController.stockView);
-
-module.exports = router;
